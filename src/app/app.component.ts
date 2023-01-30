559594f099becf451971a9e576806e2a9481335c
Harshad <gohilharshad508@gmail.com>
2023-01-30 15:20:03 +0530

    new one

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'blog';
+  userData:any = {};
+
+  getData(data:NgForm) {
+    console.warn(data);
+    this.userData=data;
+  }
 }
