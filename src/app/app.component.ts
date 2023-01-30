4bfc3808319eceebbdcd6678d6cdb6855ed5e8a9
Harshad <gohilharshad508@gmail.com>
2023-01-30 17:54:24 +0530

    change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl,FormGroup,Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,10 +7,69 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   title = 'blog';
-  userData:any = {};
 
-  getData(data:NgForm) {
-    console.warn(data);
-    this.userData=data;
+
+
+  loginForm = new FormGroup({
+    user:new FormControl('',[Validators.required,Validators.email]),
+    password:new FormControl('',[Validators.required,Validators.minLength(5)])
+  })
+  loginUser(){
+    console.log(this.loginForm.value)
+
   }
+
+
+  get user(){
+    return this.loginForm.get('user')
+  }
+
+  get password(){
+    return this.loginForm.get('password')
+  }
+
+  // name:any;
+
+
+
+  
+  // userDetials = [
+  //   {
+  //     name: 'Harshad',
+  //     email: 'h@gmail.com',
+  //     phon: '8888'
+  //   },
+  //   {
+  //     name: 'mohit',
+  //     email: 'm@gmail.com',
+  //     phon: '9999'
+  //   },
+  //   {
+  //     name: 'kartik',
+  //     email: 'k@gmail.com',
+  //     phon: '999'
+  //   }
+  // ]
+
+
+
+
+
+  // userData:any = {};
+  // getData(data:NgForm) {
+  //   console.warn(data);
+  //   this.userData=data;
+  // }
+  // list:any=[];
+
+  // addTask(item:string){
+
+  //   console.log(item)
+  //   // item=this.list;
+  //   this.list.push({id:this.list.length,name:item}) 
+  // }
+  // removeTask(id:number){
+  //   console.log(id)
+  //   this.list=this.list.filter(item=>item.id!=id)
+  // }
 }
